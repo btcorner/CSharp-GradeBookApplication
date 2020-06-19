1536153ac0a2915b8a8b26a6a74c5d3b2d4292b1
btcorner <btcorner@hotmail.co.uk>
2020-06-19 19:14:38 +0100

    Module3.Commit1
    >Added override method for GetLetterGrade
    > Uses min & max grades in gradebook to calculate score bands

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using GradeBook.Enums;
 
@@ -11,5 +13,27 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            double min = Students.Min(x => x.AverageGrade);
+            double max = Students.Max(x => x.AverageGrade);
+            double diff = max - min;
+
+            switch (averageGrade)
+            {
+                case double a when a >= (min + (diff * 0.8)):
+                    return 'A';
+                case double a when a >= (min + (diff * 0.6)):
+                    return 'B';
+                case double a when a >= (min + (diff * 0.4)):
+                    return 'C';
+                case double a when a >= (min + (diff * 0.2)):
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
+
     }
 }
